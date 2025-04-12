5125e69180dffdba4338f2c5a2bb76f472965dce
HafizBkr <hafizinovus@gmail.com>
2025-04-12 22:56:28 +0000

    first commit for test writting

---
+++ go/sommes/add-_test.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"testing"
+)
+//test unitaire classique
+func TestAdd(t *testing.T) {
+	Obtenue:=add(12,12)
+	Attendue:=24
+
+	if Obtenue != Attendue{
+		t.Errorf("Obtenue: %v, attendue: %v", Obtenue, Attendue)
+	}
+
+}
+//test de benchmarking
+func BenchmarkAdd(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		add(12, 12) 
+	}
+}
---
+++ go/sommes/add.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(a, b int) int {
+	return a + b
+}   
+
+func main() {	
+     addition:= add(12,23)
+	fmt.Println(addition)
+}
